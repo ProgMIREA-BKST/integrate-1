f86ae3c4b529d5b7ee190c0d9e0f1e062e4adf77
alexisnov <novosselski@gmail.com>
2021-04-03 10:59:31 +0300

    Update consoleIntegrate1.cpp

--- consoleIntegrate1/consoleIntegrate1.cpp
+++ consoleIntegrate1/consoleIntegrate1.cpp
@@ -1,8 +1,9 @@
-﻿// consoleIntegrate1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// consoleIntegrate1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+//Версия: v2
 #include <iostream>
 #include <stdio.h>
+#include <windows.h>
 float f1(float x) {
 	return pow(2, x);
 }
@@ -30,6 +31,16 @@ float trapIntegrate(int x1, int x2, float dx, float f(float)) {
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	HWND hwnd;
+	char Title[1024];
+	
+	GetConsoleTitle(Title, 1024); // Узнаем имя окна
+	hwnd = FindWindow(NULL, Title); // Узнаем hwnd окна
+	HDC hdc = GetDC(hwnd);
+	RECT rc;
+	GetClientRect(hwnd, &rc);
+	int iWidth = rc.right;
+	int iHeight = rc.bottom;
 	//Пределы интегрирования
 	int x1 = 0;
 	int x2 = 5;
@@ -44,6 +55,7 @@ int main()
 	printf("функция 2: метод прямоугольников: %f\n", I);
 	 I = trapIntegrate(x1, x2, dx, f2);
 	printf("функция 2: метод трапеций: %f\n", I);
+	SetPixel(hdc, iWidth / 2, iHeight / 2, RGB(255, 0, 0)); //Установка пикселя
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
